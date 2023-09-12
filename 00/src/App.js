ca293e7b382d99920fe8dbf3880a6ff9570977ad
otmorales23 <oliverwills0227@gmail.com>
2023-09-11 18:51:11 -0700

    added several react components - ran into link err

--- 00/src/App.js
+++ 00/src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Switch } from 'react-router-dom';
+import Header from './components/Header';
+import Nav from './components/Nav';
+import About from './components/About';
+import Examples from './components/Examples';
+import Contact from './components/Contact';
+// import Footer from './components/Footer';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Nav />
+      <Router>
+        <div>
+            <Nav />
+            <Routes>
+              <Route 
+                path="/About" 
+                element={<About />} 
+              />
+              <Route
+              path="Examples"
+              element={<Examples />}
+              />
+              <Route 
+                path="/Contact" 
+                element={<Contact />} 
+              />
+            </Routes>
+        </div>
+      </Router>
+      <Header />
+      <About />
+      {/* <Examples /> */}
+      <Contact />
+      {/* <Footer /> */}
     </div>
   );
 }
---
+++ 00/src/components/Examples.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Examples () {
+    return (
+        <div className="examples">
+
+        </div>
+    )
+}
+
+
+export default Examples;
